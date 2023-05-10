0910a33cbc537ae687bce9dbf7093b16d3b217a3
hongnguyen0711 <nguyenhongnguyen40@gmail.com>
2023-05-10 20:07:42 +0700

    ss6

---
+++ Thực hành Modul1/Bài tập/ss6_javaScript/exersice/area.js
@@ -0,0 +1,33 @@
+
+/*-let inputtoan;
+let inputly;
+let inputhoa;
+inputtoan = prompt("nhập điểm toán:");
+inputly = prompt("nhập điểm lý");
+inputhoa = prompt("nhập điểm hóa");
+let toan = parseFloat(inputtoan);
+let ly = parseFloat(inputly);
+let hoa = parseFloat(inputhoa);
+let sum = toan + ly + hoa;
+average = (toan+ly+hoa)/3;
+
+document.write("sum =" + sum )
+document.write("</br>")
+document.write("average:"+ average)
+
+let inputc;
+inputc = prompt("nhập vào độ c:");
+let c = parseFloat(inputc);
+f = (9*c)/5 + 32;
+document.write("độ f là:" + f);
+
+let r;
+let pi;
+pi = 3.14;
+r = prompt("Nhập bán kính:");
+C = 2*pi*r;
+S = pi*r*r;
+document.write("Chu vi hình tròn là:")
+document.write("Diện tích hình tròn là:");*/
+
+
